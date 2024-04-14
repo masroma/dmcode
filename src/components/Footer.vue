676d73ab93b69c4aebe6b52fc1670c03dab87268
unknown <masroma75@gmail.com>
2024-04-15 04:36:16 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,41 @@
 <template>
     <div class="w-full shadow-md bg-cyan-600 py-10 px-10 ">
-        <div class="max-w-7xl grid lg:grid-cols-4 gap-10 mx-auto">
+        <div class="max-w-7xl grid lg:grid-cols-4 gap-5 mx-auto">
             <div class="flex flex-col justify-center">
-                <img src="/logoputih.png" alt="logo" class="w-1/2  lg:w-max">
+                <img src="/logoputih.png" alt="logo" class="w-1/2 lg:w-max" style=" margin-left: -12px;">
             </div>
 
             <div class="flex flex-col">
-                <h1 class="text-white font-bold text-xl mb-3">
+                <h1 class="text-white font-bold lg:text-xl mb-2 lg:mb-3">
                    Menu
                 </h1>
-                <router-link :to="{ name: 'kebijakanprivasi' }" class="text-white text-base font-semibold">Kebijakan
+                <router-link :to="{ name: 'kebijakanprivasi' }" class="text-white text-sm lg:text-base">Kebijakan
                     Privasi</router-link>
-                <router-link :to="{ name: 'syaratpenggunaan' }" class="text-white text-base font-semibold">Syarat dan
+                <router-link :to="{ name: 'syaratpenggunaan' }" class="text-white text-sm lg:text-base">Syarat dan
                     ketentuan</router-link>
-                <router-link :to="{ name: 'roadmap' }" class="text-white text-base font-semibold">Roadmap
+                <router-link :to="{ name: 'roadmap' }" class="text-white text-sm lg:text-base">Roadmap
                     Belajar</router-link>
-                <router-link :to="{ name: 'konsepbelajar' }" class="text-white text-base font-semibold">Konsep Belajar</router-link>
+                <router-link :to="{ name: 'konsepbelajar' }" class="text-white text-sm lg:text-base">Konsep Belajar</router-link>
             </div>
 
             <div class="flex flex-col">
-                <h1 class="text-white font-bold text-xl mb-3">
+                <h1 class="text-white font-bold lg:text-xl mb-2 lg:mb-3">
                     Informasi UMUM
                 </h1>
-                <router-link :to="{ name: 'home' }" class="text-white text-base font-semibold">Informasi Donasi
+                <router-link :to="{ name: 'home' }" class="text-white text-sm lg:text-base">Informasi Donasi
                     Infaq</router-link>
-                <router-link :to="{ name: 'home' }" class="text-white text-base font-semibold">Laporan Donasi
+                <router-link :to="{ name: 'home' }" class="text-white text-sm lg:text-base">Laporan Donasi
                     Infaq</router-link>
             </div>
 
             <div class="flex flex-col">
-                <h1 class="text-white font-bold text-xl mb-3">
+                <h1 class="text-white font-bold lg:text-xl mb-2 lg:mb-3">
                     Informasi DMCODE
                 </h1>
-                <p class="text-white">
+                <p class="text-white text-sm lg:text-base">
                     Alamat : Ruko No 1 LT 2, Jalan wijaya kusuma RT 03 RW 05 Pondok miri gunung sindur Kab bogor
                 </p>
-                <p class="text-white">
+                <p class="text-white text-sm lg:text-base">
                     Whatsapp : 082299995502 ( chat only )
                 </p>
             </div>
