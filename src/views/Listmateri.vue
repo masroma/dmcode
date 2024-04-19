62e739de498af34527c222e9c2fc8e6adf42468a
unknown <masroma75@gmail.com>
2024-04-19 11:54:14 +0700

    update

--- src/views/Listmateri.vue
+++ src/views/Listmateri.vue
@@ -43,10 +43,10 @@
 
                     <div @click="changeTab('assets')"
                         :class="{ 'border-b-2 border-cyan-600': tabactive === 'assets', 'cursor-pointer': tabactive !== 'assets' }">
-                        assets </div>
+                        Assets </div>
                     <div @click="changeTab('diskusi')"
                         :class="{ 'border-b-2 border-cyan-600': tabactive === 'diskusi', 'cursor-pointer': tabactive !== 'diskusi' }">
-                        diskusi </div>
+                        Tanya instruktur </div>
                 </div>
 
 
@@ -84,7 +84,7 @@
 
                 <div class="bg-white shadow-lg p-5 w-full" v-if="tabactive == 'assets'">
                     <h2 class="text-xl font-semibold capitalize">
-                        assets
+                        Assets
                     </h2>
                     <div v-for="index in 3" :key="index" class="flex bg-cyan-100 px-3 py-3 justify-between my-5">
                         <p class="text-cyan-600 font-semibold">Resource code </p>
@@ -94,23 +94,93 @@
 
                 </div>
 
-                <div class="bg-white shadow-lg p-5 w-full" v-if="tabactive == 'diskusi'">
-                    <h2 class="text-xl font-semibold">
-                        diskusi
-                    </h2>
-                    <p class="text-sm my-2">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae nemo sequi
-                        iusto pariatur amet
-                        consectetur corrupti consequatur accusantium eligendi illo, quibusdam possimus esse veniam
-                        nihil
-                        ea
-                        explicabo a libero quo?
-                    </p>
+                <div class="flex flex-col gap-y-5 w-full" v-if="tabactive == 'diskusi'">
+                    <div class="bg-white shadow-lg p-5">
+                        <h2 class="text-xl font-semibold">
+                            Tanya instruktur
+                        </h2>
+                        <div class="flex flex-col gap-y-5 my-5">
+                            <div class="flex flex-col gap-y-2">
+                                <p class="text-sm text-cyan-600 font-semibold">Pertanyaan </p>
+                                <textarea class="bg-gray-50 w-full rounded-lg border-2 border-gray-100 p-2"
+                                    placeholder="Tuliskan Problem Kalian "></textarea>
+                            </div>
+                            <div class="flex flex-col gap-y-2">
+                                <p class="text-sm text-cyan-600 font-semibold">Gambar</p>
+                                <input type="file" class="bg-gray-50 w-full rounded-lg border-2 border-gray-100 p-2">
+                            </div>
+
+                            <div class="flex flex-col gap-y-2">
+                                <p class="text-sm text-cyan-600 font-semibold">Tautan link</p>
+                                <input type="text" class="bg-gray-50 w-full rounded-lg border-2 border-gray-100 p-2"
+                                    placeholder="Tautan link kode">
+                            </div>
+
+                            <button class="w-full bg-cyan-600 text-white font-smibold p-2 rounded-lg hover:bg-cyan-500">
+                                Kirim Pertanyaan
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="bg-white shadow-lg p-5">
+                        <h2 class="text-xl font-semibold">
+                            List Pertanyaan
+                        </h2>
+                        <div class="flex flex-col gap-y-5 my-5">
+                            <div class="bg-gray-100 p-3 rounded-lg">
+                                <div class="flex justify-between items-center">
+                                    <div class="flex items-center gap-x-2">
+                                        <img src="https://via.placeholder.com/100x100" class="h-8 w-8 rounded-full"
+                                            alt="pp">
+                                        <p class="text-sm font-semibold"> alam saputra</p>
+                                    </div>
+                                    <p class="text-sm font-semibold"> 24 Maret 2024</p>
+                                </div>
+                                <p class="my-3 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit.
+                                    Voluptate
+                                    assumenda quae
+                                    animi. Excepturi totam labore reiciendis porro nemo sequi! A, perspiciatis! Corrupti
+                                    soluta, quasi dicta dignissimos quod reiciendis similique odio.</p>
+                            </div>
+
+                            <div class="ms-5 bg-cyan-100 p-3 rounded-lg">
+                                <div class="flex justify-between items-center">
+                                    <div class="flex items-center gap-x-2">
+                                        <img src="https://via.placeholder.com/100x100" class="h-8 w-8 rounded-full"
+                                            alt="pp">
+                                        <p class="text-sm font-semibold"> Instruktur</p>
+                                    </div>
+                                    <p class="text-sm font-semibold"> 24 Maret 2024</p>
+                                </div>
+                                <p class="my-3 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit.
+                                    Voluptate
+                                    assumenda quae
+                                    animi. Excepturi totam labore reiciendis porro nemo sequi! A, perspiciatis! Corrupti
+                                    soluta, quasi dicta dignissimos quod reiciendis similique odio.</p>
+                            </div>
+
+                            <div class="ms-5 bg-gray-100 p-3 rounded-lg">
+                                <div class="flex justify-between items-center">
+                                    <div class="flex items-center gap-x-2">
+                                        <img src="https://via.placeholder.com/100x100" class="h-8 w-8 rounded-full"
+                                            alt="pp">
+                                        <p class="text-sm font-semibold"> Instruktur</p>
+                                    </div>
+                                    <p class="text-sm font-semibold"> 24 Maret 2024</p>
+                                </div>
+                                <p class="my-3 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit.
+                                    Voluptate
+                                    assumenda quae
+                                    animi. Excepturi totam labore reiciendis porro nemo sequi! A, perspiciatis! Corrupti
+                                    soluta, quasi dicta dignissimos quod reiciendis similique odio.</p>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
-
-
-
             </main>
         </div>
 
